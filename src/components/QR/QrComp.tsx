08789f98446a69434329679ac639621d929a93ee
lumanaa <lssuwal@gmail.com>
2024-08-15 14:44:51 +0545

    box shadow

--- src/components/QR/QrComp.tsx
+++ src/components/QR/QrComp.tsx
@@ -1,5 +1,24 @@
+import qr from "../../assets/images/image-qr-code.png";
+
+import "./qrcomp.scss";
+
 const QrComp = () => {
-  return <div className='qr-body'>QrComp</div>;
+  return (
+    <div className='qr-body'>
+      <div className='qr-section'>
+        <div className='qr-container'>
+          <img src={qr} alt='qr-code' />
+          <div className='qr-text'>
+            <h2>Improve your front-end skills by building projects</h2>
+            <p>
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QrComp;
